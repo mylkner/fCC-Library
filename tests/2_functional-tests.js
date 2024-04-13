b9a3c142fef5468f4ebdba9c19bff808e2615d10
mylkner <mylofaulkner27@gmail.com>
2024-04-13 09:57:19 +0200

    comment out example test

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -19,7 +19,7 @@ suite("Functional Tests", function () {
      * ----[EXAMPLE TEST]----
      * Each test should completely test the response of the API end-point including response status code!
      */
-    test("#example Test GET /api/books", function (done) {
+    /*test("#example Test GET /api/books", function (done) {
         chai.request(server)
             .get("/api/books")
             .end(function (err, res) {
@@ -42,7 +42,7 @@ suite("Functional Tests", function () {
                 );
                 done();
             });
-    });
+    });*/
     /*
      * ----[END of EXAMPLE TEST]----
      */
